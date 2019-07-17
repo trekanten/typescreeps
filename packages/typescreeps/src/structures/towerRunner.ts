9a978a0e6d92c4f9d419e78d4e9039447deea2d2
trekanten <martinusishoel@live.no>
2019-07-17 15:17:35 +0200

    Target hostile with most HEAL

--- packages/typescreeps/src/structures/towerRunner.ts
+++ packages/typescreeps/src/structures/towerRunner.ts
@@ -1,5 +1,9 @@
 import { getTowers, getLowestRepairTarget } from '@/creep/getters';
 
+function getHealingPower(creep: Creep) {
+  return creep.body.filter(bp => bp.type === HEAL).length;
+}
+
 export function towerRunner() {
   for (const key in Game.rooms) {
     const room = Game.rooms[key];
@@ -8,7 +12,13 @@ export function towerRunner() {
 
     const hostiles = room.find(FIND_HOSTILE_CREEPS);
     if (hostiles.length > 0) {
-      towers.forEach(tower => tower.attack(hostiles[0]));
+      let mainTarget = hostiles[0];
+      for (const hostile of hostiles) {
+        if (getHealingPower(hostile) > getHealingPower(mainTarget)) {
+          mainTarget = hostile;
+        }
+      }
+      towers.forEach(tower => tower.attack(mainTarget));
       return;
     }
 
